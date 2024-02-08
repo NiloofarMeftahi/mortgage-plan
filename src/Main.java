79013cd0d2c13c34d1aebaca729595386e398c9f
NiloofarMeftahi <niloofar.m1991@gmail.com>
2024-02-08 16:04:56 +0200

    searching names implemented

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        String searchItem = "Claes Månsson";
+        String customerToSearch = searchItem.replaceAll("[,\\s]", "").trim().toLowerCase();
         try {
             File file = new File("./prospects.txt");
             Scanner scanner = new Scanner(file);
-
             String[] headers = scanner.nextLine().split(",");
-            for (String header : headers) {
-                System.out.println(header);
-            }
+            boolean customerFound = false;
             while (scanner.hasNextLine()){
                 String data = scanner.nextLine();
-                String[] customerData = data.split(",");
-                for (String customer : customerData) {
-                    System.out.println(customer);
+                String[] customerData = data.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                String preparedCustomerName = customerData[0].replaceAll("[,\\s]", "").trim().toLowerCase();
+
+                if (preparedCustomerName.contains(customerToSearch)){
+                    customerFound = true;
+                    double totalLoan = Double.parseDouble(customerData[1].trim());
+                    double interest = Double.parseDouble(customerData[2].trim());
+                    int years = Integer.parseInt(customerData[3].trim());
+//                    double years = Double.parseDouble(customerData[3].trim()); ?
+                    System.out.println("Customer: " + customerData[0].trim());
+                    System.out.println("Total Loan: " + totalLoan);
+                    System.out.println("Interest: " + interest);
+                    System.out.println("Years: " + years);
+                    break;
+
                 }
+
+
+
+            }
+            if(!customerFound) {
+                System.err.println("Customer not found.");
             }
 
 
