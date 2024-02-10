46507a5610cad517191aea381aef488b593e127a
NiloofarMeftahi <niloofar.m1991@gmail.com>
2024-02-10 21:48:43 +0200

    added choice between showing all customers or search names

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,31 @@ import java.util.Scanner;
 
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner userInputScanner = new Scanner(System.in);
         boolean continueSearch = true; // var check for continuing the program
+        boolean showAll;
+        //System.out.println(showInfo(userInputScanner));
+        String searchItem;
 
         while (continueSearch){
-            String searchItem = enterName(userInputScanner); /* ask for the name of the customer */
+            String userChoice = showInfo(userInputScanner);
+            if (userChoice.equalsIgnoreCase("search")) {
+                searchItem = enterName(userInputScanner); /* ask for the name of the customer */
+                showAll = false;
+            }
+            else {
+                searchItem = userChoice; /* so if user put exit at the beginning program stops */
+                showAll = true;
+            }
+
             if (searchItem.equalsIgnoreCase("exit")) {  /* if entered exit, stop the program */
                 break;
             }
 
             try {
-                searchCustomer(searchItem); /* search for the entered name */
+                searchCustomer(searchItem, showAll); /* search for the entered name */
             } catch (FileNotFoundException e) {  /* if file not found stop the program */
                 continueSearch = false;
             }
@@ -38,8 +51,26 @@ public class Main {
 
         return searchItem;
     }
+    private static String showInfo(Scanner scanner) {
+        String choice;
+        //boolean userChoice = true;
+        do {
+            /*
+             * ask if the user wants to access all customer info or search
+             */
+            System.out.println("Do you want to see all data or search? Type all/search . Enter exit to stop: ");
+            choice = scanner.nextLine().trim();
 
-    private static void searchCustomer (String searchItem) throws FileNotFoundException{
+        } while (choice.isEmpty() || !choice.equalsIgnoreCase("all")
+                && !choice.equalsIgnoreCase("search")
+                && !choice.equalsIgnoreCase("exit"));
+
+
+        return choice;
+    }
+
+    private static void searchCustomer (String searchItem, boolean showAll) throws FileNotFoundException{
+        int prospectNum = 0;
         String customerToSearch = removeDiacritics(searchItem)
                 .replaceAll("[,\\s-_\\*]+", "").trim().toLowerCase();
         /*
@@ -60,29 +91,18 @@ public class Main {
                 /*
                 * do not split the data if it is in double quotation
                  */
-                String preparedCustomerName = removeDiacritics(customerData[headerIndices.get("Customer")])
-                        .replaceAll("[,\\s-_\\*]", "").trim().toLowerCase();
+                if (customerData.length >= headers.length) { /* check if customerData contains enough elements */
+                    String preparedCustomerName = removeDiacritics(customerData[headerIndices.get("Customer")])
+                            .replaceAll("[,\\s-_\\*]", "").trim().toLowerCase();
 
-                if (!preparedCustomerName.isEmpty() && preparedCustomerName.contains(customerToSearch)){
-                    customerFound = true;
-                    double totalLoan = Double.parseDouble(customerData[headerIndices.get("Total loan")].trim());
-                    double interest = Double.parseDouble(customerData[headerIndices.get("Interest")].trim());
-                    int years = Integer.parseInt(customerData[headerIndices.get("Years")].trim());
-//                    double years = Double.parseDouble(customerData[3].trim()); ?
-                    System.out.println("Customer: " + customerData[headerIndices.get("Customer")]
-                            .replaceAll("[,\\s\"]", " ").trim());
-                    /*
-                    * prepare customer data for printing correctly and print it
-                     */
-                    System.out.println("Total Loan: " + totalLoan);
-                    System.out.println("Interest: " + interest);
-                    System.out.println("Years: " + years);
-                    double monthlyPayment = monthlyPayment(totalLoan, interest, years * 12);
-                    /*
-                    * calculate the monthly payment
-                    */
-                    System.out.println("Monthly Payment: " + monthlyPayment);
+                    if ((!preparedCustomerName.isEmpty() && preparedCustomerName.contains(customerToSearch) && !showAll)
+                            || (!preparedCustomerName.isEmpty() && showAll)){
+                        customerFound = true;
+                        printCustomerData(customerData, headerIndices, ++prospectNum);
+
+                    }
                 }
+
             }
             if(!customerFound) {
                 System.out.println("Customer Not Found ");
@@ -93,6 +113,30 @@ public class Main {
             System.err.println("File not found: " + e.getMessage());
             throw e;
         }
+    }
+
+    private static void printCustomerData(String[] customerData, Map<String, Integer> headerIndices, int prospectNumber) {
+        String customer = customerData[headerIndices.get("Customer")]
+                .replaceAll("[,\\s\"]", " ").trim();
+        /*
+         * prepare customer data for printing
+         */
+        double totalLoan = Double.parseDouble(customerData[headerIndices.get("Total loan")].trim());
+        double interest = Double.parseDouble(customerData[headerIndices.get("Interest")].trim());
+        int years = Integer.parseInt(customerData[headerIndices.get("Years")].trim());
+//                    double years = Double.parseDouble(customerData[3].trim()); ?
+//        System.out.println("Customer: " + customer );
+//
+//        System.out.println("Total Loan: " + totalLoan);
+//        System.out.println("Interest: " + interest);
+//        System.out.println("Years: " + years);
+        double monthlyPayment = monthlyPayment(totalLoan, interest, years * 12);
+        /*
+         * calculate the monthly payment
+         */
+//        System.out.println("Monthly Payment: " + monthlyPayment);
+        System.out.println("Prospect " + prospectNumber + " " + customer + " wants to borrow " + totalLoan + " € for a period of "
+                + years + " years and pay approximately " + monthlyPayment + " € each month.");
     }
     private static Map<String, Integer> getHeaderIndices(String[] headers) {
         Map<String, Integer> headerIndices = new HashMap<>();
